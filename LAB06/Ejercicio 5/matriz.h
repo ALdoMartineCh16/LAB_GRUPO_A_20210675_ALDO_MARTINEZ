4b612ea9f1d05216331a88c73ae9727a22d69a87
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-14 00:37:25 -0500

    Create matriz.h

---
+++ LAB06/Ejercicio 5/matriz.h
@@ -0,0 +1,16 @@
+#ifndef MATRIZ_H
+#define MATRIZ_H
+#include<iostream>
+
+using namespace std;
+
+class Matriz{
+    private:
+        int matz[3][3];
+    public:
+        Matriz();
+        ~Matriz();
+        void buscar();
+};
+
+#endif
