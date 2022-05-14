8d34aa2ea4e8060868962059e1fe2e69d2c75486
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-13 23:30:54 -0500

    Create Area.h

---
+++ LAB06/Ejercicio 1/Area.h
@@ -0,0 +1,21 @@
+#ifndef AREA_H
+#define AREA_H
+#include<iostream>
+
+using namespace std;
+
+class area{
+	private:
+    	int altura;
+    	int largo;
+   	 
+	public:
+    	area(int,int);
+    	~area();
+    	void perimetro();
+    	void area_();
+};
+
+
+
+#endif
