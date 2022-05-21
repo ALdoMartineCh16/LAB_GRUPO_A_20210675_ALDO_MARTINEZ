920fc411bbceb79783528bc43cde295fb1d5ac9f
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-21 10:22:10 -0500

    Create Persona.h

---
+++ LAB07/Ejercicio_1./Persona.h
@@ -0,0 +1,17 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Persona{
+protected:
+	string nombre;
+	int edad;
+public:
+	Persona(string,int);
+	void mostrar();
+};
+
+#endif
+
