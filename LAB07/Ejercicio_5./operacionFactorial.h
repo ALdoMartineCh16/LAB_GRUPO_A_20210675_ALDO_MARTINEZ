289fd019eccabe2dc80f82d787613ed7cc8374cb
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-22 04:00:30 -0500

    Update operacionFactorial.h

--- LAB07/Ejercicio_5./operacionFactorial.h
+++ LAB07/Ejercicio_5./operacionFactorial.h
@@ -7,13 +7,3 @@ public:
 };
 
 #endif
-operacionFactorial.cpp
-#include "operacionFactorial.h"
-
-int operacionFactorial::devolverFactorial(){
-	int pro=1;
-	for(int i=getOperador();i!=0;i--){
-		pro*=i;
-	}
-	return pro;
-}
