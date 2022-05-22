3f98f239bc1e2a11116b0a5db69bedc10fd6b6d1
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-22 03:47:53 -0500

    Create Material.h

---
+++ LAB07/Ejercicio_2./Material.h
@@ -0,0 +1,16 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Material{
+protected:
+	string mater;
+public:
+	Material(string);
+	~Material();
+};
+
+
+#endif
