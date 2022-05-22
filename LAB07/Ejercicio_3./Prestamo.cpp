4dd08950de3dc21f9b8f0bf755764e0bdecd34bc
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-22 03:52:23 -0500

    Create Prestamo.cpp

---
+++ LAB07/Ejercicio_3./Prestamo.cpp
@@ -0,0 +1,12 @@
+#include "Prestamo.h"
+
+Prestamo::Prestamo(string a,double b,int c) : ProductoBancario(a,b,c) {
+	
+}
+
+Prestamo::~Prestamo() {
+	
+}
+void Prestamo::imprimir_(){
+	imprimir();
+}
