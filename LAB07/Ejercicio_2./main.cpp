a85f555fcfeedd73951cf2f0f18bfea5ab57743d
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-05-22 03:44:54 -0500

    Update main.cpp

--- LAB07/Ejercicio_2./main.cpp
+++ LAB07/Ejercicio_2./main.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include "Objeto.h"
+
+using namespace std;
+
+int main(){
+	string obj, material;
+	int r,g,b;
+	cout<<"Iniciando el programa...\nObjeto: ";getline(cin,obj);
+	cout<<"Hecho de : ";getline(cin,material);
+	do{
+		cout<<"\nIngresando colores(Valores que se pueden ingresar: 0 y 1): "<<endl;
+		cout<<"Red(0=0, 255=1): ";cin>>r;
+		cout<<"Green(0=0, 255=1): ";cin>>g;
+		cout<<"Blue(0=0, 255=1): ";cin>>b;
+	} while((r !=0 and r!=1) || (g!=0 and g!=1) || (b!=0 and b!=1));
+	Objeto obj1 = Objeto(r,g,b,obj,material);
+	obj1.imprimir();
+	return 0;
+}
 
