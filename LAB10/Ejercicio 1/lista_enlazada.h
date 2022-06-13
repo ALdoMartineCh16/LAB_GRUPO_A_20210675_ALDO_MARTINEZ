44f2bc69518f8b00d8c7ee61214c325e3106ca3f
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-06-13 00:01:33 -0500

    Update lista_enlazada.h

--- LAB10/Ejercicio 1/lista_enlazada.h
+++ LAB10/Ejercicio 1/lista_enlazada.h
@@ -11,7 +11,7 @@ private:
 	
 public:
 	Nodo(){}
-	void agregar_final(Nodo *&lista, int n){
+	void agregar(Nodo *&lista, int n){
 		Nodo *nuevo = new Nodo();
 		Nodo *nuevo1 = new Nodo();
 		nuevo->dato = n;
@@ -27,79 +27,13 @@ public:
 		}
 		cout<<"\tNodo ingresado."<<endl;
 	}
-	
-	void agregar_inicio(Nodo *&lista, int n){
-		Nodo *nuevo = new Nodo();
-		nuevo->dato = n;
-		nuevo->sig = lista;
-		lista = nuevo;
-	}
-	void insertar_posicion(Nodo *&lista, int n, int pos){
-		Nodo *nuevo = new Nodo();
-		Nodo *nuevo1 = new Nodo();
-		nuevo->dato=n;
-		nuevo1=lista;
-		for(int i=1;nuevo1!=NULL;i++){
-			if(i==pos){
-				nuevo->sig=nuevo1->sig;
-				nuevo1->sig=nuevo;
-				return;
-			}
-			nuevo1=nuevo1->sig;
-		}
-	}
-	
-	void eliminar_ultimo(Nodo *&lista){
-		Nodo *inicio = lista;
-		Nodo *inicio1 = lista;
-		if(lista != NULL){
-			while(inicio->sig !=NULL){
-				inicio1 = inicio;
-				inicio = inicio->sig;
-			}
-			if(inicio != inicio1){
-				inicio1->sig = NULL;
-				delete inicio;
-			}else{
-				delete lista;
-				lista=NULL;
-			}
-		}else{
-			cout<<"Lista vacia"<<endl;
-		}
 		
-	}
-	
-	void eliminar_inicio(Nodo *&lista){
-		Nodo *inicio = lista;
-		Nodo *inicio1 = lista;
-		if(lista != NULL){
-			if(inicio->sig !=NULL){
-				inicio1 = inicio;
-				inicio = inicio->sig;
-				lista = lista->sig;
-			}
-			if(inicio != inicio1){
-				inicio1->sig = NULL;
-				delete inicio1;
-			}else{
-				delete lista;
-				lista=NULL;
-			}
-		}else{
-			cout<<"Lista vacia"<<endl;
-		}
-	}
-	void mostrar(Nodo *lista){
-		if(lista==NULL){
-			cout<<"Lista vacia"<<endl;
-		}else{
+		
+		void mostrar(Nodo *lista){
 			while(lista != NULL){
 				cout<<lista->dato<<"->";
 				lista = lista->sig;
 			}
 		}
-		
-	}
 };
 #endif
