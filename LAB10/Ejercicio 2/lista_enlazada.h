d43971ddb6183c132143007d7089e1c097d9bcb3
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-06-13 00:00:05 -0500

    Create lista_enlazada.h

---
+++ LAB10/Ejercicio 2/lista_enlazada.h
@@ -0,0 +1,45 @@
+#ifndef LISTA_ENLAZADA_H
+#define LISTA_ENLAZADA_H
+#include <iostream>
+#include <stdlib.h>
+ 
+using namespace std;
+class Nodo{
+private:
+	int dato;
+	Nodo *sig;
+	
+public:
+	Nodo(){}
+	void agregar_final(Nodo *&lista, int n){
+		Nodo *nuevo = new Nodo();
+		Nodo *nuevo1 = new Nodo();
+		nuevo->dato = n;
+		nuevo->sig = NULL;
+		if(lista==NULL){
+			lista = nuevo;
+		}else{
+			nuevo1 = lista;
+			while(nuevo1->sig!=NULL){
+				nuevo1=nuevo1->sig;
+			}
+			nuevo1->sig = nuevo;
+		}
+		cout<<"\tNodo ingresado."<<endl;
+	}
+	
+	void agregar_inicio(Nodo *&lista, int n){
+		Nodo *nuevo = new Nodo();
+		nuevo->dato = n;
+		nuevo->sig = lista;
+		lista = nuevo;
+	}
+		
+	void mostrar(Nodo *lista){
+		while(lista != NULL){
+			cout<<lista->dato<<"->";
+			lista = lista->sig;
+		}
+	}
+};
+#endif
