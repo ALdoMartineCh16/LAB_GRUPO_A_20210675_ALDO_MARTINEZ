b610ecf1e01983c8b6928e36e4536a7e17c7df90
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-06-17 23:53:21 -0500

    Create main.cpp

---
+++ LAB11/Ejercicio_2/main.cpp
@@ -0,0 +1,29 @@
+#include "stack.h"
+ 
+int main(){
+	int data, num, opc;
+	Nodo *lista = NULL;
+	do{
+	    cout<<"MENU:"<<endl;
+	    cout<<"1. Agregar elemento."<<endl;
+	    cout<<"2. Eliminar elemento."<<endl;
+	    cout<<"3. Mostras pila."<<endl;
+	    cout<<"4. Salir."<<endl;
+	    cout<<"Opcion: ";cin>>opc;
+	    
+	    if(opc==1){
+	        cout<<"N: ";cin>>data;
+	        lista->agregar(lista,data);
+	    }else if(opc==2){
+	        lista->eliminar(lista);
+	    }else if(opc==3){
+	        lista->mostrar(lista);
+	    }else if(opc==4){
+	        break;
+	    }else{
+	        cout<<"\nOpcion incorrecta."<<endl;
+	    }
+	}while(true);
+	
+	return 0;
+}
