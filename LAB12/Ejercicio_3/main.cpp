e0573f7a7f71127a3565fbf2ad591c06c36525f7
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-06-19 18:19:04 -0500

    Create main.cpp

---
+++ LAB12/Ejercicio_3/main.cpp
@@ -0,0 +1,35 @@
+#include "Cola.h"
+ 
+using namespace std;
+ 
+int main(){
+    Nodo *Cola = new Nodo();
+    int dato,opc;
+    do{
+        cout<<"\t.:Menu:."<<endl;
+        cout<<"1. Insertar elementos. "<<endl;
+        cout<<"2. Mostrar elementos. "<<endl;
+        cout<<"3. Eliminar un elemento."<<endl;
+        cout<<"4. Buscar elemento."<<endl;
+        cout<<"5. Salir."<<endl;
+        cout<<"Opcion: ";cin>>opc;
+        
+        if(opc==1){
+            cout<<"\nElemento que desea ingresar: ";cin>>dato;
+            Cola->agregar(dato);
+        }else if(opc==2){
+            Cola->mostrar();
+        }else if(opc==3){
+            Cola->eliminar();
+        }else if(opc==4){
+            Cola->buscar();
+        }else if(opc==5){
+            break;
+        }else{
+            cout<<"Opcion incorrecta."<<endl;
+        }
+    }while(true);
+    
+    
+    return 0;
+}
