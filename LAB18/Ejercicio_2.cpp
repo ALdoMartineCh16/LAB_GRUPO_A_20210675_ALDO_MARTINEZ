ef50d776da7b7411560932f961d1b70bbf26c122
ALdoMartineCh16 <86008861+ALdoMartineCh16@users.noreply.github.com>
2022-08-06 23:07:35 -0500

    Create Ejercicio_2.cpp

---
+++ LAB18/Ejercicio_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+template<int n>
+struct Fibonacci {
+	enum{
+		value = Fibonacci<n-1>::value + Fibonacci<n-2>::value
+	};
+};
+
+template <>
+struct Fibonacci<1>
+{
+	enum{ value = 1 };
+};
+
+template <>
+struct Fibonacci<0>
+{
+	enum{ value = 0 };
+};
+
+int main()
+{
+	cout<<Fibonacci<5>::value;
+	return 0;
+}
